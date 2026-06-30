53c5c53b9216accec78a7b9ebe133dfefcf19d1b
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-06-30 15:56:39 +0530

    Add function to compute a raised to the power of b

---
+++ Mathematics/a_raised_to_power_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+
+    long long result = 1;
+
+    while (b > 0) {
+        if (b % 2 != 0) {
+            result = result * a;
+            b = b - 1;
+        }
+        b = b / 2;
+        a = a * a;
+    }
+
+    cout << result;
+
+    return 0;
+}
